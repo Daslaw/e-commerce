afa843001d2398113519133b100775e361abc4df
Daslaw <daslaw26@gmail.com>
2023-06-10 12:33:45 +0100

    e-commerce project

--- README.md
+++ README.md
@@ -1,7 +1,49 @@
-# Getting Started with Create React App
+# Frontend Mentor - E-commerce product page
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+This is a solution to the [E-commerce product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+
+## Overview
+
+This challenge was gotten from Frontend Mentor and facilitated by Andrew James Okpainmo.
+
+**To do this challenge, you need a basic understanding of HTML, CSS, JavaScript, TailwindCSS and React.**
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+
+### Screenshot
+
+![]()
+
+### Links
+
+- Solution URL: (https://github.com/Daslaw/multi_step_form.git)
+- Live Site URL:(https://daslaw.github.io/multi_step_form/)
+
+## My process
+
+- Write and Format the Project with Create-React-App
+- Styling with TailwindCSS
+- Pushed and hosted on GITHUB
+
+### Built with
+
+- - [React](https://reactjs.org/) - JS library
+- TailwindCSS
+### Tools
+- VScode
+- Chrome Browser
+
+### What I learned
+
+- [React](https://reactjs.org/) - JS library
+- TailwindCSS
 ## Available Scripts
 
 In the project directory, you can run:
@@ -15,56 +57,12 @@ The page will reload when you make changes.\
 You may also see any lint errors in the console.
 
 ### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
 ### `npm run eject`
 
 **Note: this is a one-way operation. Once you `eject`, you can't go back!**
 
 If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+## Acknowledgments
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+**Thanks to Andrew James Okpainmo**🚀
--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import { data } from './data'
+import React from 'react'
+import logo from "./images/logo.svg"
+import avatar from "./images/image-avatar.png"
+import {FaCartPlus} from "react-icons/fa"
+import {FaChevronCircleLeft, FaChevronCircleRight} from "react-icons/fa"
+import minus from "./images/icon-minus.svg"
+import plus from "./images/icon-plus.svg"
 
-function App() {
+
+// Header Section
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <header className="flex items-center justify-between p-8 border-bottom border-2 max-w-7xl max-auto">
+        <div className='flex items-center justify-start gap-4'>
+          <img src={logo} alt="logo" />
+          <nav className='hidden'>
+            <ul className='flex items-center justify-start gap-4'>
+              <li>Collections</li>
+              <li>Men</li>
+              <li>Women</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <ul className='flex items-center justify-start gap-4'>
+            <li>
+                <button>
+                  <FaCartPlus className='text-2xl text-slate-600'/>
+                </button>
+              </li>
+            <li><img src={avatar} alt="avatar" className='w-12'/></li>
+          </ul>
+        </div>
+    </header>
+    </>
+  )
 }
 
+function App() {
+  const [products] = useState(data);
+  const [value, setValue] = useState(0)
+
+  const [amount, setAmount] = useState(0)
+  const [slideIndex, setSlideIndex] = useState(1)
+  
+  const {mainImage} = products[value]
+
+  const nextSlide = () => {
+      if (slideIndex !== products.length) {
+        setSlideIndex(slideIndex + 1)
+    } else if (slideIndex === products.length){
+        setSlideIndex(1)
+  };
+}
+  const previousSlide = () =>{
+    if (slideIndex !== 1)  {
+          setSlideIndex(slideIndex - 1)
+      } else if (slideIndex === 1){
+        setSlideIndex (products.length)
+      }
+  }
+  
+  const HandleMinus = () => {
+    setAmount(amount - 1)
+    if (amount <= 0 ) setAmount (0)
+  };
+  
+  return (
+    <>  
+      <Header/>
+      <section className='max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 lg:mt-10'>
+        <article>
+       <div className='relative'>
+         <img src={mainImage} alt="" className='w-full lg:rounded-2xl'/>
+         <ul>
+          <li><button onClick={previousSlide} className='bg-white rounded-full p-5 shadow left-0 absolute left-4 top-1/2 -translate-y-1/2'><FaChevronCircleLeft /></button></li>
+          <li><button onClick={nextSlide} className='bg-white rounded-full absolute p-5 shadow right-4 top-1/2 -translate-y-1/2'><FaChevronCircleRight /></button></li>
+         </ul>
+       </div>
+        <ul className='hidden lg:flex items-center justify-start gap-5 flex-wrap mt-5'>
+          {products.map((item, index) =>( 
+              <li key={item.id} onClick={() => setValue(index)} className={`${index === value && "border-2 border-orange-400 opacity-80"} border-2 rounded-2xl overflow-hidden cursor-pointer`}
+              >
+                <img src={item.thumbnail} alt="" className='w-20 rounded-xl' />
+              </li>
+        ))}
+        </ul>
+        </article>
+          {/* The text Area  */}
+        <article className='px-8 pb-10'>
+          <h2 className='bg-slate-100 py-1 px2 text-orange-400 uppercase tracking-wide text-sm font-bold inline-block rounded shadow mb-10'>Sneaker Company</h2>
+          <h1 className='text-slate-900 mb-10 font-bold text-3xl lg:text-4xl'>Fall Limited Edition Sneakers</h1>
+          <p className='text-slate-60 mb-10 loading-relaxed'>
+              These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.
+          </p>
+            <div className='flex flex-wrap items-center justify-between'>
+              <ul className='flex items-center gap-5'>
+                <li className='text-slate-900 font-bold'>$125.00</li>
+                <li className='bg-orange-100 py-1 px2 text-orange-400 uppercase tracking-wide text-sm font-bold inline-block rounded shadow mb-10'>50%</li>
+              </ul>
+              <p className='text-slate-600 text-sm'><s>$250.00</s></p>
+            </div>
+            <div className='mt-10'>
+              <ul className='flex items-center justify-between bg-slate-100 py-2 px-4 rounded shadow'>
+                <li onClick={HandleMinus} className='cursor-pointer' >
+                  <img src={minus} alt="" />
+                </li>
+                <li>{amount}</li>
+                <li onClick={() => setAmount(amount + 1)} className='cursor-pointer'><img src={plus} alt="" /></li>
+              </ul>
+            <div className='text-center mx-auto'>
+                <button className='flex items-center justify-center gap-4 bg-orange-500 py-2 px-4 w-full text-white font-bold rounded-lg shadow mt-5 w-full'>
+                <FaCartPlus/> Add to cart
+              </button>
+            </div>
+            </div>
+        </article>
+      </section>
+    </>
+  );
+}
 export default App;
